7a56b32b01e79b4e14c7cd87f88c62c6f564b4ef
Jose Salcedo <jose.salcedo.386@my.csun.edu>
2024-11-02 20:58:28 +0000

    update

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.example.repository;
 
+import org.springframework.stereotype.Repository;
+// import service.AccountService;
+@Repository
 public interface AccountRepository {
+    // Account accountEntity = new Account();
+    //new user registration
+    
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,16 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+// import repository.AccountRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.AccountRepository;
+@Service
 public class AccountService {
+    AccountRepository accountRepo; 
+    //registerAccount
+    @RequestMapping("entity/Account")
+    public Account registerAccount(){
+        return null;
+    }
 }
